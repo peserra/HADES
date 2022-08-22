9b95712eedb5ee3eecc0955979c9a13a236c3f19
peserra <pedro.occulate@gmail.com>
2022-08-22 15:23:40 -0300

    classe analise e analista <temporario>

--- Teste/hades_teste/src/main/java/hades/Analise.java
+++ Teste/hades_teste/src/main/java/hades/Analise.java
@@ -3,36 +3,94 @@ package hades;
 import java.util.LinkedList;
 import java.util.List;
 
-public  class Analise {
+public class Analise {
     private Cliente cliente;
     private Analista analista;
-    private double resultado;
+    private String resultado;
     private String classificacao;
     private List<Double> analisadas;
-    
+    private int pontuacao = 0;
 
-    public Analise (Cliente cliente, Analista analista) {
+    public Analise(Analista analista, Cliente cliente) {
         this.cliente = cliente;
         this.analista = analista;
-        
+
     }
 
-    public double analisar(Cliente cliente) {
+    public String analisar(Cliente cliente) {
         analisadas = new LinkedList<Double>();
         if (analista != null) {
-            for (Double transacao: cliente.transacoesMes) { //como entrar nos dados de cliente ?
+            for (Double transacao : cliente.transacoesMes) { // como entrar nos dados de cliente ?
                 analisadas.add(transacao);
             }
         }
-        
-        
+        double somaValores = 0;
+        for (Double transacao : analisadas) {
+            somaValores += transacao;
+        }
+
+        // Criterios de analise
+        try {
+            // criterio valor das transacoes
+            switch (cliente.getRisco()) {
+                case "baixo":
+                    if (somaValores > (cliente.getMovTotalMesAnterior() * 1.2)) {
+                        pontuacao++;
+                    }
+                case "medio":
+                    if (somaValores > (cliente.getMovTotalMesAnterior() * 1.15)) {
+                        pontuacao++;
+                    }
+                case "alto":
+                    if (somaValores > (cliente.getMovTotalMesAnterior() * 1.1)) {
+                        pontuacao++;
+                    }
+            }
+
+            // analise paraiso
+            if (cliente.isParaiso() && somaValores > 0) {
+                pontuacao++;
+            }
+
+            // faturamento
+            if (somaValores > cliente.getFaturamento()) {
+                pontuacao++;
+            }
+
+            // criterio quantidade das analises
+            switch (cliente.getRisco()) {
+                case "baixo":
+                    if (analisadas.size() > (cliente.getNumTransacoes() * 1.2)) {
+                        pontuacao++;
+                    }
+                case "medio":
+                    if (analisadas.size() > (cliente.getNumTransacoes() * 1.15)) {
+                        pontuacao++;
+                    }
+                case "alto":
+                    if (analisadas.size() > (cliente.getNumTransacoes() * 1.1)) {
+                        pontuacao++;
+                    }
+            }
+
+            resultado = classificar(pontuacao);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            resultado = "Não foi possível analisar, tente novamente";
+        } finally {
+            System.out.println("Fim da analise");
+        }
         return resultado;
+
     }
 
-    //mudar metodo para usar dentro de analisar
-    private String classificar(double resultado) {
-        resultado = analisar(cliente);
-        //determina a classificação a partir da analise
+    private String classificar(int pontuacao) {
+        classificacao = "";
+        if (pontuacao > 0 && pontuacao <= 2)
+            classificacao = "Alpha";
+        else
+            classificacao = "Omega";
         return classificacao;
     }
 }
--- Teste/hades_teste/src/main/java/hades/Cliente.java
+++ Teste/hades_teste/src/main/java/hades/Cliente.java
@@ -8,12 +8,66 @@ public class Cliente {
     private String cnpj;
     private String risco;
     public List<Double> transacoesMes;
-    public double movTotalMesAnterior;
+    private double movTotalMesAnterior;
     private double mediaValorMesAnterior;
-    public double numTransacoes;
-    public boolean paraiso;
-    public double faturamento;
+    private double numTransacoes;
+    private boolean paraiso;
+    private double faturamento;
 
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCnpj() {
+        return cnpj;
+    }
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+    public String getRisco() {
+        return risco;
+    }
+    public void setRisco(String risco) {
+        this.risco = risco;
+    }
+    public List<Double> getTransacoesMes() {
+        return transacoesMes;
+    }
+    public void setTransacoesMes(List<Double> transacoesMes) {
+        this.transacoesMes = transacoesMes;
+    }
+    public double getMovTotalMesAnterior() {
+        return movTotalMesAnterior;
+    }
+    public void setMovTotalMesAnterior(double movTotalMesAnterior) {
+        this.movTotalMesAnterior = movTotalMesAnterior;
+    }
+    public double getMediaValorMesAnterior() {
+        return mediaValorMesAnterior;
+    }
+    public void setMediaValorMesAnterior(double mediaValorMesAnterior) {
+        this.mediaValorMesAnterior = mediaValorMesAnterior;
+    }
+    public double getNumTransacoes() {
+        return numTransacoes;
+    }
+    public void setNumTransacoes(double numTransacoes) {
+        this.numTransacoes = numTransacoes;
+    }
+    public boolean isParaiso() {
+        return paraiso;
+    }
+    public void setParaiso(boolean paraiso) {
+        this.paraiso = paraiso;
+    }
+    public double getFaturamento() {
+        return faturamento;
+    }
+    public void setFaturamento(double faturamento) {
+        this.faturamento = faturamento;
+    }
     public void solicitarAberturaDeConta();
     public void solicitarFechamentoDeConta();
     public void solicitarTrocaTerritorio();
